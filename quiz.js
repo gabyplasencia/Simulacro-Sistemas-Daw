ba1ad31e0eb550f745b5cd5ebd22c374dd6d5056
Gabriela <gabyplasencia1995@gmail.com>
2023-06-02 13:26:43 +0100

    Se agrego un contador de aciertos

--- quiz.js
+++ quiz.js
@@ -3,12 +3,17 @@
         qtnContainerSelector: '.wrapper',
         awsSelector: '.aws',
         resetButtonSelector: '.btn',
-        containerSelector:'.container'
+        containerSelector:'.container',
+        resultSelector: '.result',
+        resultTotalSelector: '.total',
+        resultScoreSelector: '.score'
     };
 
     const quiz = () => {
         const init = () => {
             const questions = document.querySelectorAll(selectors.qtnContainerSelector);
+            let result = document.querySelector(selectors.resultSelector)
+            let score = result.querySelector(selectors.resultScoreSelector)
             questions?.forEach((qtn) => {
                 const answers = qtn.querySelectorAll(selectors.awsSelector);
                 answers?.forEach((aws) => {
@@ -17,6 +22,9 @@
                         if(!qtn.hasAttribute("data-done")){       
                             if(aws.hasAttribute("data-right")){
                                 aws.classList.add("right");
+                                let current = parseInt(score.innerText);
+                                current++
+                                score.innerText = current
                             }else{
                                 aws.classList.add("incorrect");
                                 rightAws.classList.add("right")
@@ -32,7 +40,13 @@
                     cleanQuestions(questions);
                 })
             }
+            initResultTotal(questions)
         };
+        const initResultTotal = (questions) => {
+            let result = document.querySelector(selectors.resultSelector)
+            let total = result.querySelector(selectors.resultTotalSelector)
+            total.innerText = questions.length
+        }
         const cleanQuestions = (questions) =>{
             questions?.forEach((qtn) =>{
                 qtn.removeAttribute("data-done");
@@ -43,17 +57,24 @@
                 }); 
             });
 			reorderQuestions(questions);
+            clearScore()
 			window.scrollTo({ top: 0, behavior: 'smooth' });
         }
-
+        const clearScore = () => {
+            let result = document.querySelector(selectors.resultSelector)
+            let score = result.querySelector(selectors.resultScoreSelector)
+            score.innerText = 0
+        }
 		const reorderQuestions = (questions) =>{
 			const container = document.querySelector(selectors.containerSelector);
 			const $button = container.lastElementChild;
+            const result = container.querySelector(selectors.resultSelector)
 			container.innerHTML = "";
 			questions = Array.from(questions).sort(() => Math.random() - 0.5);
 			questions?.forEach(qtn => {
 				container.appendChild(qtn);
 		    });
+            container.appendChild(result)
 			container.appendChild($button);
 			
 		};
